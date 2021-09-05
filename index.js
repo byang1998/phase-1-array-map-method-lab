fd7ea64908b6628915134b954b84425aa1539f31
Ben Yang <benyang2020@gmail.com>
2021-09-04 20:32:19 -0400

    commit tree 8a97cfa29e80a16c1fe4a70fb5d7dc638e0e1ecb

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+
+
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +15,19 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+//invoke means how to execute the function 
+function titleCased(){
+    
+
+  return tutorials.map(sentence => {
+    //.split creates a comma at every space, can also create a comma at specified pattern. EX: after every o ("o")
+    let words = sentence.split(" ");
+    console.log(words)
+    let newwords = words.map(word => {
+       word.slice(0,1).toUpperCase() + word.slice(1)
+       return titleCased()
+       
+    } )
+    });
+  };
+
